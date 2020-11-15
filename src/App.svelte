8b68000c207f32a9df24d103112cd585a02f3134
Sequeira <asequeir@PMINTL.NET>
2020-11-15 13:13:01 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Output from "./Output.svelte";
+	export let goal = "be number 1";
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
+
+<main>
+	Write your goal!
+	<input type="text" bind:value={goal} />
+	<Output userGoal={goal} />
+</main>
---
+++ src/Output.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let result;
+  export let userGoal;
+ 
+    
+    $: result = userGoal.includes('!'); //re-run this code whenever any of the referenced values change
+    
+</script>
+
+<main>
+	<h3 class="{result ? 'isWrong' : ''}"> {userGoal} </h3>
+</main>
+
+<style>
+	.isWrong{
+		color:red;
+	}
+</style>
